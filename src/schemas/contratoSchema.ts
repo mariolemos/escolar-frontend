97b81fcd9f757e3e8e30474fc494273e0df04abf
mariolemos <lemosmm64@gmail.com>
2026-08-08 15:39:03 -0300

    Feito ajuste no responsavelShema, para verificar cpf já cadastrado

--- src/schemas/contratoSchema.ts
+++ src/schemas/contratoSchema.ts
@@ -2,50 +2,53 @@ import { formatToCurrency, parseCurrencyToNumber } from "@/utils/formatMoeda";
 import path from "path";
 import z, { number, refine } from "zod";
 
-export const contratoFormSchema = z.object({
-  valorContratual: z    
-    .string()    
-    .refine(val => {return formatToCurrency(val)})
-    .transform(val => parseCurrencyToNumber(val)),
-              
-  dataInicial: z
-    .coerce.date()
-    .optional()
-    .refine((val) => val !== undefined && val !== null, {
-      message: "Nascimento é obrigatório",
-    })
-    .refine(
-      (val) => {
-        if (!val) return true;
-        const today = new Date();
-        today.setHours(0, 0, 0, 0);
-        return val <= today;
-      },
-      {
-        message: "Nascimento deve ser uma data",
-      },
-    ),    
-  dataFinal: z
-    .coerce.date()
-    .optional()
-    .refine((val) => val !== undefined && val !== null, {
-      message: "Nascimento é obrigatório",
-    }),        
-  responsavelId: z
-    .string()
-    .min(1, "Responsavel é obrigatório")
-    .max(3, "Respponsavel deve ter no máximo 3 caracteres"),
-  ativo: z.boolean("true"),  
-})
-.refine((dados: any ) => dados.dataFinal >= dados.dataInicial, {
-      message: "Data final não pode ser anterior a data inicial",
-      path: ['dataFinal'],
-    });
+export const contratoFormSchema = z
+  .object({
+    valorContratual: z
+      .string()
+      .refine((val) => {
+        return formatToCurrency(val);
+      })
+      .transform((val) => parseCurrencyToNumber(val)),
+
+    dataInicial: z.coerce
+      .date()
+      .optional()
+      .refine((val) => val !== undefined && val !== null, {
+        message: "Nascimento é obrigatório",
+      })
+      .refine(
+        (val) => {
+          if (!val) return true;
+          const today = new Date();
+          today.setHours(0, 0, 0, 0);
+          return val <= today;
+        },
+        {
+          message: "Nascimento deve ser uma data",
+        },
+      ),
+    dataFinal: z.coerce
+      .date()
+      .optional()
+      .refine((val) => val !== undefined && val !== null, {
+        message: "Nascimento é obrigatório",
+      }),
+    responsavelId: z
+      .string()
+      .min(1, "Responsavel é obrigatório")
+      .max(3, "Respponsavel deve ter no máximo 3 caracteres"),
+    ativo: z.boolean("true"),
+  })
+  .refine((dados: any) => dados.dataFinal >= dados.dataInicial, {
+    message: "Data final não pode ser anterior a data inicial",
+    path: ["dataFinal"],
+  });
 
 export type ContratoFormSchema = z.infer<typeof contratoFormSchema>;
 
 export const contratoFormDefaultValues: ContratoFormSchema = {
-  valorContratual: 0,  
+  valorContratual: 0,
   dataInicial: new Date(),
   dataFinal: new Date(),
   responsavelId: "",
--- src/schemas/responsavelSchema.ts
+++ src/schemas/responsavelSchema.ts
@@ -2,45 +2,70 @@ import { isValidCPF } from "@/utils/isValidCPF";
 import z from "zod";
 import { enderecoDefaultValues, enderecoSchema } from "./enderecoSchema";
 import { contatoSchema } from "./contatoSchema";
+import { apiGet } from "@/services/api";
 
-export const responsavelFormSchema = z.object({
-  nome: z
-    .string()
-    .min(1, "Nome é obrigatório")
-    .max(50, "Nome deve conter no máximo 50 caracteres"),
-  dataNascimento: z
-    .date()
-    .optional()
-    .refine((val) => val !== undefined && val !== null, {
-      message: "Nascimento é obrigatório",
-    })
-    .refine(
-      (val) => {
-        if (!val) return true;
-        const today = new Date();
-        today.setHours(0, 0, 0, 0);
-        return val <= today;
-      },
-      {
-        message:
-          "Nascimento deve ser uma data válida e não pode ser maior que hoje",
-      },
-    ),
-  cpf: z
-    .string()
-    .min(1, "CPF é obrigatório")
-    .refine(isValidCPF, { message: "CPF inválido" }),
-  rg: z
-    .string()
-    .min(1, "RG é obrigatório")
-    .max(10, "RG deve ter no máximo 10 caracteres"),
-  parentesco: z
-    .string()
-    .min(1, "Parentesco deve ser Informado")
-    .max(20, "Parentesco deve conter no máximo 20 caracteres"),
-  endereco: enderecoSchema,
-  contatos: z.array(contatoSchema),
-});
+export const responsavelFormSchema = z
+  .object({
+    nome: z
+      .string()
+      .min(1, "Nome é obrigatório")
+      .max(50, "Nome deve conter no máximo 50 caracteres"),
+    dataNascimento: z
+      .date()
+      .optional()
+      .refine((val) => val !== undefined && val !== null, {
+        message: "Nascimento é obrigatório",
+      })
+      .refine(
+        (val) => {
+          if (!val) return true;
+          const today = new Date();
+          today.setHours(0, 0, 0, 0);
+          return val <= today;
+        },
+        {
+          message:
+            "Nascimento deve ser uma data válida e não pode ser maior que hoje",
+        },
+      ),
+    cpf: z
+      .string()
+      .min(11, "CPF é obrigatório")
+      .refine(isValidCPF, { message: "CPF inválido" }),
+    rg: z
+      .string()
+      .min(1, "RG é obrigatório")
+      .max(10, "RG deve ter no máximo 10 caracteres"),
+    parentesco: z
+      .string()
+      .min(1, "Parentesco deve ser Informado")
+      .max(20, "Parentesco deve conter no máximo 20 caracteres"),
+    endereco: enderecoSchema,
+    contatos: z.array(contatoSchema),
+  })
+  .superRefine(async (res, num) => {
+    try {
+      const response = await apiGet(`/responsavel/consultarCpf?cpf=${res.cpf}`,
+      );
+      console.log("********", response);
+      // response.success = true;
+      console.log("###", response.success);
+      const resJson = await response.success
+
+      console.log("&&&&, res, num")
+      console.log("%%%", response.success)
+
+      if (!resJson) {
+        num.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Este CPf já está cadastrado no sistema.",
+          path: ["cpf"],
+        });
+      }
+    } catch (error) {
+      console.log("mmmmmm");
+    }
+  });
 
 export type ResponsavelFormSchema = z.infer<typeof responsavelFormSchema>;
 
